250adb043cb736e49ca0903d14bacea599c9a771
gelissa <gelissaleveille@yahoo.com>
2019-09-10 09:49:55 -0400

    simplified code, created one onclick

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,80 @@
 // set my variables
 
 // create a variable for the number to guess
-var numGuess = 145; 
+// var numGuess = 145; 
+    var numGuess = Math.floor(Math.random() * 120);
 // create onclick for each incremental guess
 $(".guessNum").text(numGuess);
 
+$("#amethyst").attr("data-id", Math.floor(Math.random() * 12) +1)
+$("#steven").attr("data-id", Math.floor(Math.random() * 12) +1)
+$("#ruby").attr("data-id", Math.floor(Math.random() * 12) +1)
+$("#sapphire").attr("data-id", Math.floor(Math.random() * 12) +1)
+
 // create variable for the counter
 var counter = 0;
-// create onclick event for when each crystal gem is clicked
-$("#amethyst").on("click", function() {
-    // alert("amethyst was clicked");
-    counter += 20;
+
+$(".crystal").on("click", function() {
+    var currentNum = $(this).data("id")
+    console.log(currentNum)
+    counter += currentNum;
     $(".userGuess").text(counter);
     if (counter === numGuess){
         $(".displayText").text("You won!");
     }else if (counter >= numGuess){
         $(".displayText").text("Oops, missed it");
     }
-    // gameEnd();
 });
-$("#steven").on("click", function(){
-    // alert("steven was clicked");
-    counter += 100
-    $(".userGuess").text(counter);
-    if (counter === numGuess){
-        $(".displayText").text("You won!");
-    }else if (counter >= numGuess){
-        $(".displayText").text("Oops, missed it");
-    }
-    // gameEnd();
-});
-$("#ruby").on("click", function(){
-    // alert("ruby was clicked");
-    counter += 15
-    $(".userGuess").text(counter);
-    if (counter === numGuess){
-        $(".displayText").text("You won!");
-    }else if (counter >= numGuess){
-        $(".displayText").text("Oops, missed it");
-    }
-    // gameEnd();
-});
-$("#sapphire").on("click" , function(){
-    // alert("sapphire was clicked");
-    counter += 10
-    $(".userGuess").text(counter);
-    if (counter === numGuess){
-        $(".displayText").text("You won!");
-    }else if (counter >= numGuess){
-        $(".displayText").text("Oops, missed it");
-    }
-    // gameEnd();
-});
+
+
+
+
+
+// // create onclick event for when each crystal gem is clicked
+// $("#amethyst").on("click", function() {
+//     // alert("amethyst was clicked");
+//     counter += 20;
+//     $(".userGuess").text(counter);
+//     if (counter === numGuess){
+//         $(".displayText").text("You won!");
+//     }else if (counter >= numGuess){
+//         $(".displayText").text("Oops, missed it");
+//     }
+//     // gameEnd();
+// });
+// $("#steven").on("click", function(){
+//     // alert("steven was clicked");
+//     counter += 100
+//     $(".userGuess").text(counter);
+//     if (counter === numGuess){
+//         $(".displayText").text("You won!");
+//     }else if (counter >= numGuess){
+//         $(".displayText").text("Oops, missed it");
+//     }
+//     // gameEnd();
+// });
+// $("#ruby").on("click", function(){
+//     // alert("ruby was clicked");
+//     counter += 15
+//     $(".userGuess").text(counter);
+//     if (counter === numGuess){
+//         $(".displayText").text("You won!");
+//     }else if (counter >= numGuess){
+//         $(".displayText").text("Oops, missed it");
+//     }
+//     // gameEnd();
+// });
+// $("#sapphire").on("click" , function(){
+//     // alert("sapphire was clicked");
+//     counter += 10
+//     $(".userGuess").text(counter);
+//     if (counter === numGuess){
+//         $(".displayText").text("You won!");
+//     }else if (counter >= numGuess){
+//         $(".displayText").text("Oops, missed it");
+//     }
+//     // gameEnd();
+// });
 // make each crystal have a value
 
 // if they reach the number, alert a win
